d4fc4b357b1fa8d6eadaf480db05f46f3124cdcf
tony <sai1432teja1432@gmail.com>
2024-10-30 21:12:20 +0530

    req

--- todo/settings.py
+++ todo/settings.py
@@ -246,6 +246,18 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'django',
+#         'USER': 'root',
+#         'PASSWORD': 'mysql',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
